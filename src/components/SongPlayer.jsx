90531d51e2e2a945ef2fc550dc007d1b3f728288
Marshall Brown <marshallendel@gmail.com>
2023-04-16 13:59:09 -0400

    Add music data

--- src/components/SongPlayer.jsx
+++ src/components/SongPlayer.jsx
@@ -1,20 +1,20 @@
-import {FaPlay, FaAngleLeft, FaAngleRight} from 'react-icons/fa'
+import { FaPlay, FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 const SongPlayer = () => {
-    return (
-        <div className="song-controls">
-            <div className="time-control">
-                <p>Start Time</p>
-                <input type="range" name="" id="" />
-                <p>End Time</p>
-            </div>
-            <div className="play-control">
-                <FaAngleLeft size='32px'/>
-                <FaPlay size='32px'/>
-                <FaAngleRight size='32px' />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="song-controls">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" name="" id="" />
+        <p>End Time</p>
+      </div>
+      <div className="play-control">
+        <FaAngleLeft size="32px" />
+        <FaPlay size="32px" />
+        <FaAngleRight size="32px" />
+      </div>
+    </div>
+  );
+};
 
-export default SongPlayer
+export default SongPlayer;
