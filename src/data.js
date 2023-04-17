829ed82b32d19a44bc029f7be7b0fd5fe504bc0c
Marshall Brown <marshallendel@gmail.com>
2023-04-16 21:51:22 -0400

    Change order

--- src/data.js
+++ src/data.js
@@ -22,18 +22,40 @@ function musicData() {
       id: crypto.randomUUID(),
       active: false,
     },
-    
+
     {
       name: "Eleven",
-      cover:
-        "https://f4.bcbits.com/img/a3644381725_10.jpg",
+      cover: "https://f4.bcbits.com/img/a3644381725_10.jpg",
       artist: "Last Dinosaurs",
-      audio: "https://drive.google.com/uc?export=download&id=1p2noC-AvNF0Sh0QLDZD1G7ZQPRCEUl8P",
+      audio:
+        "https://drive.google.com/uc?export=download&id=1p2noC-AvNF0Sh0QLDZD1G7ZQPRCEUl8P",
       color: ["#fbb726", "#495ca9"],
       id: crypto.randomUUID(),
       active: false,
     },
-    
+    {
+      name: "Home",
+      cover:
+        "https://static.qobuz.com/images/covers/jc/r0/adi4tv2ipr0jc_600.jpg",
+      artist: "Medasin",
+      audio:
+        "https://drive.google.com/uc?export=download&id=11cXY2dZJljWS5G0KYMCmdCk1iXhwM3FL",
+      color: ["#b03959", "#264419"],
+      id: crypto.randomUUID(),
+      active: false,
+    },
+
+    {
+      name: "Neo Surf",
+      cover:
+        "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/247231735_296480552301624_4122189635686925292_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=z9ezU8d3L_IAX-bYO8w&_nc_ht=scontent-ord5-2.xx&oh=00_AfANXTKdQ3NBo0Kwdd3TIkE5yALZlQ9TrMiNelzUexfhvQ&oe=6440B312",
+      artist: "GENER8ION, 070 Shake",
+      audio:
+        "https://drive.google.com/uc?export=download&id=16SrHp_KH0j2vZ8sNBeEvU2yV7_Spbzgr",
+      color: ["#000000", "#40b4e3"],
+      id: crypto.randomUUID(),
+      active: false,
+    },
     {
       name: "Nightfall",
       cover:
@@ -44,26 +66,7 @@ function musicData() {
       id: crypto.randomUUID(),
       active: false,
     },
-    {
-      name: "Neo Surf",
-      cover:
-        "https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/247231735_296480552301624_4122189635686925292_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=z9ezU8d3L_IAX-bYO8w&_nc_ht=scontent-ord5-2.xx&oh=00_AfANXTKdQ3NBo0Kwdd3TIkE5yALZlQ9TrMiNelzUexfhvQ&oe=6440B312",
-      artist: "GENER8ION, 070 Shake",
-      audio: "https://drive.google.com/uc?export=download&id=16SrHp_KH0j2vZ8sNBeEvU2yV7_Spbzgr",
-      color: ["#000000", "#40b4e3"],
-      id: crypto.randomUUID(),
-      active: false,
-    },
-    {
-      name: "Home",
-      cover:
-        "https://static.qobuz.com/images/covers/jc/r0/adi4tv2ipr0jc_600.jpg",
-      artist: "Medasin",
-      audio: "https://drive.google.com/uc?export=download&id=11cXY2dZJljWS5G0KYMCmdCk1iXhwM3FL",
-      color: ["#b03959", "#264419"],
-      id: crypto.randomUUID(),
-      active: false,
-    },
+
     {
       name: "Under the City Stars",
       cover:
@@ -73,7 +76,7 @@ function musicData() {
       color: ["#205950", "#2ab3bf"],
       id: crypto.randomUUID(),
       active: false,
-    }
+    },
   ];
 }
 
